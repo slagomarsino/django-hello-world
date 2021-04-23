dfc9bfbbb69300b41d90aa97df6da82e15260e62
Sebastian Lagomarsino <lagomarsinosebastian@gmail.com>
2021-04-23 17:42:31 -0300

    Added params in index view in hello_world app

--- django/hello_world/views.py
+++ django/hello_world/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     """Placeholder index view"""
-    return render(request, 'index.html')
+    params = {'title': 'Hello World Django'}
+    return render(request, 'index.html', params)
